5c0edd681fb8354a6502f1738aeb65c4bc636416
NikolayNS <dmitrenko.ns@gmail.com>
2022-01-19 10:07:45 +0300

    fix kafka config

--- src/main/kotlin/com/dmitrenko/userworker/config/KafkaConfig.kt
+++ src/main/kotlin/com/dmitrenko/userworker/config/KafkaConfig.kt
@@ -3,6 +3,7 @@ package com.dmitrenko.userworker.config
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,6 +14,7 @@ import org.springframework.kafka.core.*
 import org.springframework.kafka.listener.ContainerProperties
 import org.springframework.kafka.listener.SeekToCurrentErrorHandler
 import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
 import org.springframework.util.backoff.FixedBackOff
 
 @EnableKafka
@@ -37,8 +39,8 @@ class KafkaConfig(
 	fun <M> producerFactory(): ProducerFactory<String, M> {
 		val props = HashMap<String, Any>()
 		props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
-		props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-		props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+		props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+		props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
 		return DefaultKafkaProducerFactory(props)
 	}
 
